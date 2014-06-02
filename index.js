5ba882a185c302efdd56d0ec3fc93c21fc8558b3
Duncan Overbruck <overbruck@mir.de>
2014-06-02 17:31:57 +0200

    added random steps etc

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 var Baaar = require('baaar');
 
-module.exports = function(instance) {
-    if(!instance instanceof Baaar) instance = new Baaar();
+module.exports = function(bar) {
+    if(!(bar instanceof Baaar)) bar = new Baaar();
+    var n = 0;
+    var end = false;
+
+    var work = function() {
+        setTimeout(function() {
+            if(end) return;
+            n = clamp(n + Math.round(Math.random() * 15), 1, 100);
+            bar.set(n);
+            work();
+        }, 800);
+    };
 
     return function(request) {
-        request.on('progress', function(e) {
-            console.log('progress', e);
-            //instance.set();
+        request.on('request', function(e) {
+            end = false;
+            work();
+        }).on('progress', function(e) {
+            // TODO: add progress for uploads
+        }).on('end', function(e) {
+            end = true;
+            n = 0;
+            bar.set(100);
+            setTimeout(function() {
+                bar.destroy();
+            }, 800);
         });
-        request.on('end', function(e) {
-            console.log('end', e);
-            instance.destroy();
-        });
-    }
+    };
 };
+
+function clamp(n, min, max) {
+    if(n < min) return min;
+    if(n > max) return max;
+    return n;
+}
